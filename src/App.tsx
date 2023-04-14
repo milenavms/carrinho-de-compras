21d76991dfa76cfacfbe03a672df3675ed5fa513
milena <milenasantos@bemol.com.br>
2023-04-14 15:36:15 -0400

    home page grid creation
    home page grid creation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import { Container } from "@mui/system";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
       <NavBar />
-      <Outlet />
+      <Container maxWidth="lg">
+        <Outlet />
+      </Container>
     </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import * as React from "react";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: "0 px",
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  border: "none",
+  boxShadow: "none",
+}));
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Box sx={{ flexGrow: 1, paddingTop: 2 }}>
+      <Grid container spacing={2}>
+        <Grid xs={12}>
+          <Item>carrousel</Item>
+        </Grid>
+
+        <Grid xs={12}>
+          <Item>menu</Item>
+        </Grid>
+
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid xs={12} sm={4} md={3} key={index}>
+            <Item>cards dos produtos</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
